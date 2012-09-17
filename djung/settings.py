a4b035c00b7c76407b86e1a4c93f3b44a018f593
Mathieu Comandon <strycore@gmail.com>
2012-09-17 16:10:35 +0200

    More complete settings

--- djung/settings.py
+++ djung/settings.py
@@ -4,57 +4,23 @@ PROJECT_ROOT = dirname(dirname(abspath(__file__)))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ADMINS = (
-    ('Your Name', 'yourname@yourdomain.org'),
+    ('Your Name', 'yourname@example.com'),
 )
 MANAGERS = ADMINS
+SITE_ID = 1
+ROOT_URLCONF = 'djung.urls'
+WSGI_APPLICATION = 'djung.wsgi.application'
+SECRET_KEY = 'this is my super secret key'
+
+# Databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'dev.db',
     }
 }
-TIME_ZONE = 'Europe/Paris'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-MEDIA_ROOT = join(PROJECT_ROOT, 'media')
-MEDIA_URL = '/media/'
-STATIC_ROOT = join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    join(PROJECT_ROOT, 'components'),
-)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-TEMPLATE_DIRS = (
-    join(PROJECT_ROOT, 'templates'),
-)
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
 
-COMPRESS_PRECOMPILERS = (
-    ('text/coffeescript', 'coffee --compile --stdio'),
-    ('text/less', 'lessc {infile} {outfile}'),
-)
-
-ROOT_URLCONF = 'djung.urls'
-WSGI_APPLICATION = 'djung.wsgi.application'
-SECRET_KEY = 'this is my super secret key'
-
+# Apps
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +34,58 @@ INSTALLED_APPS = (
     'frontend',
 )
 
+# Localization
+TIME_ZONE = 'Europe/Paris'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static / Media files
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    join(PROJECT_ROOT, 'components'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+# Templates
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+TEMPLATE_DIRS = (
+    join(PROJECT_ROOT, 'templates'),
+)
+COMPRESS_PRECOMPILERS = (
+    ('text/coffeescript', 'coffee --compile --stdio'),
+    ('text/less', 'lessc {infile} {outfile}'),
+)
+
+# Middleware
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+# Authentication / Registration (may need Django-Registration)
+ACCOUNT_ACTIVATION_DAYS = 3
+LOGIN_REDIRECT_URL = "/"
+
+# Email
+DEFAULT_FROM_EMAIL = "admin@example.com"
+EMAIL_SUBJECT_PREFIX = "[Djung]"
+
+# Logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -76,9 +94,19 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
     'handlers': {
         'mail_admins': {
-            'level': 'ERROR',
+            'level': 'DEBUG',
+            'include_html': True,
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
