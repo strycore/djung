365e6daf9059bb0a7ca8143ebf3e90eeaf8e9f11
Mathieu Comandon <strycore@gmail.com>
2012-09-17 16:09:55 +0200

    Example model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contric.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+#class MyModel(models.Model):
+#    title = models.CharField(max_length=255)
+#    slug = models.SlugField()
+#    author = models.ForeignKey(User)
+#    content = models.TextField()
+#    created_at = models.DateTimeField(auto_now=True)
+#
+#    class Meta:
+#        ordering = ['-created_at']
+#
+#    def __unicode__(self):
+#        return self.title
+#
+#    def get_absolute_url(self):
+#        return '/my-object/%s-%d' % (self.slug, self.id)
+#
+#    def save(self, *args, **kwargs):
+#        self.slug = slugify("%s-%d" % (self.title, self.id))
