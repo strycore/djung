cedf1df3f1e71de861f2272865a5d4fe34ab6f2a
Mathieu Comandon <strycore@gmail.com>
2013-03-13 21:12:47 +0100

    Serve media files in development

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,5 @@
 """Root URLconf"""
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 #from django.views.generic import TemplateView
 
@@ -11,3 +12,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('main.urls')),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)$', 'serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
