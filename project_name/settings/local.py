4161c7db7ca3c9d25de0d32b1627c6b55c95b2e7
Mathieu Comandon <strycore@gmail.com>
2015-02-17 12:01:34 +0100

    Default to sqlite for dev settings

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -1,3 +1,4 @@
+import os
 from .base import *  # noqa
 
 INSTALLED_APPS += (
@@ -9,10 +10,15 @@ DEBUG = True
 # Databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '{{ project_name }}',
-        'USER': '{{ project_name }}',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+
+        # It is strongly suggested to switch your dev environment's database
+        # to the same engine as your production (PostgreSQL)
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': '{{ project_name }}',
+        # 'USER': '{{ project_name }}',
+        # 'PASSWORD': 'admin',
+        # 'HOST': 'localhost'
     }
 }
