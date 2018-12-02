bb1bda66b2bd2df507e6d9256e4ccbf77d4f4ead
Mathieu Comandon <strycore@gmail.com>
2018-12-02 02:24:03 -0800

    Update MIDDLEWARE setting

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -89,15 +89,15 @@ STATICFILES_FINDERS = (
 )
 
 # Middleware
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 # Authentication / Users
 AUTH_USER_MODEL = 'accounts.User'
