0e69895c0066310fdf8fa1a867584875ec80d8cd
Mathieu Comandon <strycore@gmail.com>
2018-12-02 02:24:25 -0800

    Re-enable serving files in development mode

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,7 @@
 """Root URLconf"""
 from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,8 +13,5 @@ urlpatterns = [
 ]
 
 
-# if settings.DEBUG:
-#     urlpatterns += [
-#         path(r'^media/(?P<path>.*)$', 'serve',
-#          {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-#     ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
